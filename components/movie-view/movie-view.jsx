d29876682035a2c929c26010a734fe13ce85ef06
Miriam <118216207+cotufetes@users.noreply.github.com>
2023-09-13 19:00:19 +0200

    added prop-types to movie-view and movie-card

--- components/movie-view/movie-view.jsx
+++ components/movie-view/movie-view.jsx
@@ -1,26 +1,43 @@
+import PropTypes from "prop-types";
+
 export const MovieView = ({movie, onBackClick}) => {
   return (
     <div>
       <div>
-        <img src={movie.image} />
+        <img src={movie.ImageURL} />
       </div>
       <div>
         <span>Title: </span>
-        <span>{movie.title}</span>
+        <span>{movie.Title}</span>
       </div>
       <div>
         <span>Synopsis: </span>
-        <span>{movie.synopsis}</span>
+        <span>{movie.Synopsis}</span>
       </div>
       <div>
         <span>Genre: </span>
-        <span>{movie.genre}</span>
+        <span>{movie.Genre.Name}</span>
       </div>
       <div>
         <span>Director: </span>
-        <span>{movie.director}</span>
+        <span>{movie.Director.Name}</span>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
   );
+};
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    ImageURL: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    Synopsis: PropTypes.string.isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired
 };
