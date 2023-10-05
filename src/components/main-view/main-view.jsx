531932f99fecdeded9c3b380af627e09201cb7de
Miriam <118216207+cotufetes@users.noreply.github.com>
2023-10-05 16:24:29 +0200

    added login-view component

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
 
 export const MainView = () => {
 
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     fetch("https://cotuflix-c45510e677a7.herokuapp.com/movies")
@@ -31,7 +34,9 @@ export const MainView = () => {
       });
   }, []);
 
-  const [selectedMovie, setSelectedMovie] = useState(null);
+  if (!user) {
+    return <LoginView />;
+  }
 
   if (selectedMovie) {
     return (
