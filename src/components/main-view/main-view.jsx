f523f777fcbab300df6d9471db477899e61e3cbe
Miriam <118216207+cotufetes@users.noreply.github.com>
2023-10-05 16:42:12 +0200

    added verified login data and form validation

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -35,7 +35,7 @@ export const MainView = () => {
   }, []);
 
   if (!user) {
-    return <LoginView />;
+    return <LoginView onLoggedIn={(user) => setUser(user)} />;
   }
 
   if (selectedMovie) {
